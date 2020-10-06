cdb77dab284e79e1ad1fef1486d39bdfd8a0798b
Galina Kistanova <gkistanova@gmail.com>
2020-10-06 16:53:53 +0000

    Ready for github authentication in production.

--- buildbot/osuosl/master/config/auth.py
+++ buildbot/osuosl/master/config/auth.py
@@ -1,9 +1,8 @@
+from buildbot.plugins import util
+#from twisted.python import log
+
 import config
 
-#from buildbot.www.auth import NoAuth
-from buildbot.plugins import util
-
-#from twisted.python import log
 
 def getAuth():
     # For test local setup use NoAuth instead.
@@ -24,7 +23,7 @@ def getAuthz():
             # Admins can do anything.
             # defaultDeny=False: if user does not have the admin role,
             # we continue parsing rules.
-            util.AnyEndpointMatcher(role="admin", defaultDeny=False),
+            util.AnyEndpointMatcher(role="LLVM Lab team", defaultDeny=False),
 
             # Allow authors to stop, force or rebuild their own builds,
             util.StopBuildEndpointMatcher(role="owner", defaultDeny=False),
@@ -34,27 +33,22 @@ def getAuthz():
             # allow devs to force or rebuild any build.
             util.RebuildBuildEndpointMatcher(role="owner", defaultDeny=False),
             util.RebuildBuildEndpointMatcher(role="worker-owner", defaultDeny=False),
-            util.RebuildBuildEndpointMatcher(role="llvm"),
+            util.RebuildBuildEndpointMatcher(role="LLVM Committers"),
 
             util.ForceBuildEndpointMatcher(role="owner", defaultDeny=False),
             util.ForceBuildEndpointMatcher(role="worker-owner", defaultDeny=False),
-            util.ForceBuildEndpointMatcher(role="llvm"),
+            util.ForceBuildEndpointMatcher(role="LLVM Committers"),
 
             # Future-proof control endpoints. No parsing rules beyond this.
 
             # Allows anonymous to look at build results.
-            util.AnyControlEndpointMatcher(role="admin"),
+            util.AnyControlEndpointMatcher(role="LLVM Lab team"),
         ],
         roleMatchers=[
-            #util.RolesFromGroups(groupPrefix=str(config.options.get('GitHub Auth', 'admins'))),
-            #util.RolesFromGroups(groupPrefix=str(config.options.get('GitHub Auth', 'developers'))),
-            util.RolesFromGroups(), # All authorized LLVM org members
+            util.RolesFromGroups(groupPrefix="llvm/"),
+            util.RolesFromGroups(groupPrefix="llvm/"),
             # role owner is granted when property owner matches the email of the user
             util.RolesFromOwner(role="owner"),
-            util.RolesFromUsername(
-                roles=["admin"],
-                usernames=config.options.options("Admins"),
-            ),
         ],
     )
 
