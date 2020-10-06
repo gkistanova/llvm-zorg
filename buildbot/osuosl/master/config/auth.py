e74a7aa3474106a79cfae3d346e35b0f364f9e12
Galina Kistanova <gkistanova@gmail.com>
2020-10-06 05:55:52 +0000

    Semi-working github authorization. Needs polishing an more testing.

--- buildbot/osuosl/master/config/auth.py
+++ buildbot/osuosl/master/config/auth.py
@@ -21,36 +21,38 @@ def getAuthz():
 
     authz = util.Authz(
         allowRules=[
-            util.AnyEndpointMatcher(role="viewers", defaultDeny=False),
-
             # Admins can do anything.
             # defaultDeny=False: if user does not have the admin role,
             # we continue parsing rules.
-            util.AnyEndpointMatcher(role="admins", defaultDeny=False),
+            util.AnyEndpointMatcher(role="admin", defaultDeny=False),
 
             # Allow authors to stop, force or rebuild their own builds,
             util.StopBuildEndpointMatcher(role="owner", defaultDeny=False),
             # Allow bot owners to stop, force or rebuild on their own bots,
-            util.StopBuildEndpointMatcher(role="worker-owner", defaultDeny=False),
+            util.StopBuildEndpointMatcher(role="worker-owner"),
 
-            # allow core devs to force or rebuild any build.
+            # allow devs to force or rebuild any build.
             util.RebuildBuildEndpointMatcher(role="owner", defaultDeny=False),
             util.RebuildBuildEndpointMatcher(role="worker-owner", defaultDeny=False),
-            util.RebuildBuildEndpointMatcher(role="developers"),
+            util.RebuildBuildEndpointMatcher(role="llvm"),
 
             util.ForceBuildEndpointMatcher(role="owner", defaultDeny=False),
             util.ForceBuildEndpointMatcher(role="worker-owner", defaultDeny=False),
-            util.ForceBuildEndpointMatcher(role="developers"),
+            util.ForceBuildEndpointMatcher(role="llvm"),
 
             # Future-proof control endpoints. No parsing rules beyond this.
-            util.AnyControlEndpointMatcher(role="admins"),
+
+            # Allows anonymous to look at build results.
+            util.AnyControlEndpointMatcher(role="admin"),
         ],
         roleMatchers=[
-            util.RolesFromGroups(groupPrefix="llvm/"),
+            #util.RolesFromGroups(groupPrefix=str(config.options.get('GitHub Auth', 'admins'))),
+            #util.RolesFromGroups(groupPrefix=str(config.options.get('GitHub Auth', 'developers'))),
+            util.RolesFromGroups(), # All authorized LLVM org members
             # role owner is granted when property owner matches the email of the user
             util.RolesFromOwner(role="owner"),
             util.RolesFromUsername(
-                roles=["admins"],
+                roles=["admin"],
                 usernames=config.options.options("Admins"),
             ),
         ],
