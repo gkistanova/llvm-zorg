32b86e3aa57d78cc7f98650b6e7a132be1ee9c3f
Andrei Lebedev <alebedev@accesssoftek.com>
2020-10-20 22:40:16 -0700

    Cleaned buildbot/builders/Util.

--- zorg/buildbot/builders/Util.py
+++ zorg/buildbot/builders/Util.py
@@ -1,5 +1,3 @@
-# TODO: Remove all not actually used code.
-
 from buildbot.process.results import SUCCESS
 import re
 
@@ -43,62 +41,3 @@ def extractVSEnvironment(exit_status, stdout, stderr):
     vs_env_dict = dict(l.strip().split('=',1)
         for l in stdout.split('\n') if len(l.split('=', 1)) == 2)
     return {'vs_env': vs_env_dict}
-
-def extractClangVersion(exit_status, stdout, stderr):
-    '''Helper function for SetPropertyCommand. Receives "clang --version" output
-    and returns clang_version property for ShellCommands.'''
-    if exit_status:
-        return {}
-    res = re.search(r"version\s*(\S+)", stdout)
-    if res:
-        return {'clang_version': res.group(1)}
-    return {}
-
-def getConfigArgs(origname):
-  name = origname
-  args = []
-  if name.startswith('Release'):
-    name = name[len('Release'):]
-    args.append('--enable-optimized')
-  elif name.startswith('Debug'):
-    name = name[len('Debug'):]
-  else:
-    raise ValueError('Unknown config name: %r' % origname)
-
-  if name.startswith('+Asserts'):
-    name = name[len('+Asserts'):]
-    args.append('--enable-assertions')
-  elif name.startswith('-Asserts'):
-    name = name[len('-Asserts'):]
-    args.append('--disable-assertions')
-  else:
-    args.append('--disable-assertions')
-
-  if name.startswith('+Checks'):
-    name = name[len('+Checks'):]
-    args.append('--enable-expensive-checks')
-
-  if name:
-    raise ValueError('Unknown config name: %r' % origname)
-
-  return args
-
-def _did_last_build_fail(buildstep):
-  # Grab the build number for the current build.
-  build_number = buildstep.build.build_status.number
-  # If build number is 0, there is no previous build to fail and the build
-  # directory *SHOULD* be clean. So dont clean.
-  if build_number == 0:
-    return False
-
-  # Lookup the status of the last build from the master.
-  builder = buildstep.build.builder
-  previous_build = builder.master.status.getBuilder(builder.name)\
-                                        .getLastFinishedBuild()
-
-  # If the previous build is None, do a clean build.
-  if previous_build is None:
-    return True
-
-  # If the previous builder did not succeed, do a clean build.
-  return previous_build.getResults() != SUCCESS
