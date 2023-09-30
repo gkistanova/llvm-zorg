d82556adbee7c823551dfd352481c8fbcb80cbbe
Vladimir Vereschaka <vvereschaka@accesssoftek.com>
2023-09-29 20:27:44 -0700

    Update the builder configurations to use the consolidated unified build factory.
    All appropriate builder configurations have been changed to use
    UnifiedTreeBuilder.getCmakeExBuildFactory() factory.
    
    Depended on gkistanova/llvm-zorg:#19

--- buildbot/osuosl/master/config/release_builders.py
+++ buildbot/osuosl/master/config/release_builders.py
@@ -29,20 +29,23 @@ all = [
     'tags'  : ["clang"],
     'workernames' : ["as-builder-3"],
     'builddir': "llvm-clang-x86_64-win-rel",
-    'factory' : UnifiedTreeBuilder.getCmakeWithNinjaWithMSVCBuildFactory(
+    'factory' : UnifiedTreeBuilder.getCmakeExBuildFactory(
                     vs="autodetect",
                     depends_on_projects=['llvm', 'clang'],
                     clean=True,
                     checks=[
-                    "check-llvm-unit",
-                    "check-clang-unit"],
-                    extra_configure_args=[
-                        "-DLLVM_ENABLE_WERROR=OFF",
-                        "-DLLVM_TARGETS_TO_BUILD=ARM",
-                        "-DLLVM_DEFAULT_TARGET_TRIPLE=armv7-unknown-linux-eabihf",
-                        "-DLLVM_ENABLE_ASSERTIONS=OFF",
-                        "-DLLVM_OPTIMIZED_TABLEGEN=OFF",
-                        "-DLLVM_LIT_ARGS=-v --threads=32"])},
+                        "check-llvm-unit",
+                        "check-clang-unit"
+                    ],
+                    cmake_definitions={
+                        "LLVM_ENABLE_WERROR"            : "OFF",
+                        "LLVM_TARGETS_TO_BUILD"         : "ARM",
+                        "LLVM_DEFAULT_TARGET_TRIPLE"    : "armv7-unknown-linux-eabihf",
+                        "LLVM_ENABLE_ASSERTIONS"        : "OFF",
+                        "LLVM_OPTIMIZED_TABLEGEN"       : "OFF",
+                        "LLVM_LIT_ARGS"                 : "-v --threads=32",
+                    },
+                )},
 
 # Expensive checks builders.
 
@@ -51,49 +54,55 @@ all = [
     'tags'  : ["llvm", "expensive-checks"],
     'workernames' : ["as-builder-4"],
     'builddir': "llvm-clang-x86_64-expensive-checks-ubuntu-rel",
-    'factory' : UnifiedTreeBuilder.getCmakeWithNinjaBuildFactory(
+    'factory' : UnifiedTreeBuilder.getCmakeExBuildFactory(
                     depends_on_projects=["llvm", "lld"],
                     clean=True,
-                    extra_configure_args=[
-                        "-DLLVM_ENABLE_EXPENSIVE_CHECKS=ON",
-                        "-DLLVM_ENABLE_WERROR=OFF",
-                        "-DLLVM_USE_SPLIT_DWARF=ON",
-                        "-DLLVM_USE_LINKER=gold",
-                        "-DCMAKE_BUILD_TYPE=Debug",
-                        "-DCMAKE_CXX_FLAGS=-U_GLIBCXX_DEBUG -Wno-misleading-indentation",
-                        "-DLLVM_LIT_ARGS=-vv -j32"])},
+                    cmake_definitions={
+                        "LLVM_ENABLE_EXPENSIVE_CHECKS"  : "ON",
+                        "LLVM_ENABLE_WERROR"            : "OFF",
+                        "LLVM_USE_SPLIT_DWARF"          : "ON",
+                        "LLVM_USE_LINKER"               : "gold",
+                        "CMAKE_BUILD_TYPE"              : "Debug",
+                        "CMAKE_CXX_FLAGS"               : "-U_GLIBCXX_DEBUG -Wno-misleading-indentation",
+                        "LLVM_LIT_ARGS"                 : "-vv -j32",
+                    },
+                )},
 
     {'name' : "llvm-clang-x86_64-expensive-checks-win-release",
     'tags'  : ["llvm", "expensive-checks"],
     'workernames' : ["as-worker-93"],
     'builddir': "llvm-clang-x86_64-expensive-checks-win-rel",
-    'factory' : UnifiedTreeBuilder.getCmakeWithNinjaWithMSVCBuildFactory(
+    'factory' : UnifiedTreeBuilder.getCmakeExBuildFactory(
                     vs="autodetect",
                     depends_on_projects=["llvm", "lld"],
                     clean=True,
-                    extra_configure_args=[
-                        "-DLLVM_ENABLE_EXPENSIVE_CHECKS=ON",
-                        "-DLLVM_ENABLE_WERROR=OFF",
-                        "-DCMAKE_BUILD_TYPE=Debug"])},
+                    cmake_definitions={
+                        "LLVM_ENABLE_EXPENSIVE_CHECKS"  : "ON",
+                        "LLVM_ENABLE_WERROR"            : "OFF",
+                        "CMAKE_BUILD_TYPE"              : "Debug",
+                    },
+                )},
 
     {'name' : "llvm-clang-x86_64-expensive-checks-debian-release",
     'tags'  : ["llvm", "expensive-checks"],
     'workernames' : ["gribozavr4"],
     'builddir': "llvm-clang-x86_64-expensive-checks-debian-rel",
-    'factory' : UnifiedTreeBuilder.getCmakeWithNinjaBuildFactory(
+    'factory' : UnifiedTreeBuilder.getCmakeExBuildFactory(
                     depends_on_projects=["llvm", "lld"],
                     clean=True,
-                    extra_configure_args=[
-                        "-DLLVM_CCACHE_BUILD=ON",
-                        "-DLLVM_ENABLE_EXPENSIVE_CHECKS=ON",
-                        "-DLLVM_ENABLE_WERROR=OFF",
-                        "-DCMAKE_BUILD_TYPE=Release",
-                        "-DCMAKE_CXX_FLAGS=-U_GLIBCXX_DEBUG",
-                        "-DLLVM_LIT_ARGS=-v -vv -j96"],
+                    cmake_definitions={
+                        "LLVM_CCACHE_BUILD"             : "ON",
+                        "LLVM_ENABLE_EXPENSIVE_CHECKS"  : "ON",
+                        "LLVM_ENABLE_WERROR"            : "OFF",
+                        "CMAKE_BUILD_TYPE"              : "Release",    #TODO:VV: why not Debug as all others?
+                        "CMAKE_CXX_FLAGS"               : "-U_GLIBCXX_DEBUG",
+                        "LLVM_LIT_ARGS"                 : "-v -vv -j96",
+                    },
                     env={
                         'PATH':'/home/llvmbb/bin/clang-latest/bin:/home/llvmbb/bin:/usr/local/bin:/usr/local/bin:/usr/bin:/bin',
                         'CC': 'clang', 'CXX': 'clang++',
-                    })},
+                    }
+                )},
 
 # Cross builders.
 
@@ -101,16 +110,42 @@ all = [
     'tags'  : ["clang", "llvm", "compiler-rt", "cross", "armv7"],
     'workernames' : ["as-builder-5"],
     'builddir': "x-armv7l-rel",
-    'factory' : XToolchainBuilder.getCmakeWithMSVCBuildFactory(
-                    vs="autodetect",
-                    clean=True,
-                    checks=[
-                    "check-llvm",
-                    "check-clang",
-                    "check-lld",
-                    "check-compiler-rt-armv7-unknown-linux-gnueabihf"
+    'factory' : UnifiedTreeBuilder.getCmakeExBuildFactory(
+                    depends_on_projects=[
+                        "llvm",
+                        "compiler-rt",
+                        "clang",
+                        "clang-tools-extra",
+                        "libunwind",
+                        "libcxx",
+                        "libcxxabi",
+                        "lld",
                     ],
-                    checks_on_target = [
+                    # Suppress passing LLVM_ENABLE_RUNTIMES to CMake.
+                    # We get this list from the CMake cache file.
+                    enable_runtimes=None,
+                    cmake_definitions={
+                        "CMAKE_CXX_FLAGS"               : "-D__OPTIMIZE__",
+
+                        "LLVM_TARGETS_TO_BUILD"         : "ARM",
+                        "TOOLCHAIN_TARGET_TRIPLE"       : "armv7-unknown-linux-gnueabihf",
+                        "DEFAULT_SYSROOT"               : "C:/buildbot/.arm-ubuntu",
+                        "ZLIB_ROOT"                     : "C:/buildbot/.zlib-win32",
+                        "LLVM_LIT_ARGS"                 : "-v -vv --threads=32",
+                        "REMOTE_TEST_HOST"              : util.Interpolate("%(prop:remote_test_host:-)s"),
+                        "REMOTE_TEST_USER"              : util.Interpolate("%(prop:remote_test_user:-)s"),
+                    },
+                    cmake_options=[
+                        "-C", util.Interpolate("%(prop:srcdir_relative)s/clang/cmake/caches/CrossWinToARMLinux.cmake")
+                    ],
+                    allow_cmake_defaults=True,
+                    checks=[
+                        "check-llvm",
+                        "check-clang",
+                        "check-lld",
+                        "check-compiler-rt-armv7-unknown-linux-gnueabihf"
+                    ],
+                    checks_on_target=[
                         ("libunwind",
                             ["python", "bin/llvm-lit.py",
                             "-v", "-vv", "--threads=32",
@@ -125,31 +160,56 @@ all = [
                             'runtimes/runtimes-armv7-unknown-linux-gnueabihf-bins/libcxx/test',
                             ])
                     ],
-                    extra_configure_args=[
-                        "-DLLVM_TARGETS_TO_BUILD=ARM",
-                        "-DTOOLCHAIN_TARGET_TRIPLE=armv7-unknown-linux-gnueabihf",
-                        "-DDEFAULT_SYSROOT=C:/buildbot/.arm-ubuntu",
-                        "-DZLIB_ROOT=C:/buildbot/.zlib-win32",
-                        "-DLLVM_LIT_ARGS=-v -vv --threads=32",
-                        util.Interpolate("%(prop:remote_test_host:+-DREMOTE_TEST_HOST=)s%(prop:remote_test_host:-)s"),
-                        util.Interpolate("%(prop:remote_test_user:+-DREMOTE_TEST_USER=)s%(prop:remote_test_user:-)s"),
-                    ],
-                    cmake_cache="../llvm-project/clang/cmake/caches/CrossWinToARMLinux.cmake")},
+                    vs="autodetect",
+                    install_dir="install",
+                    clean=True,
+                    env = {
+                        # TMP/TEMP within the build dir (to utilize a ramdisk).
+                        'TMP'        : util.Interpolate("%(prop:builddir)s/%(prop:objdir)s"),
+                        'TEMP'       : util.Interpolate("%(prop:builddir)s/%(prop:objdir)s"),
+                    }
+                )},
 
     {'name' : "llvm-clang-win-x-aarch64-release",
     'tags'  : ["clang", "llvm", "compiler-rt", "cross", "aarch64"],
     'workernames' : ["as-builder-6"],
     'builddir': "x-aarch64-rel",
-    'factory' : XToolchainBuilder.getCmakeWithMSVCBuildFactory(
-                    vs="autodetect",
-                    clean=True,
-                    checks=[
-                    "check-llvm",
-                    "check-clang",
-                    "check-lld",
-                    "check-compiler-rt-aarch64-unknown-linux-gnu"
+    'factory' : UnifiedTreeBuilder.getCmakeExBuildFactory(
+                    depends_on_projects=[
+                        "llvm",
+                        "compiler-rt",
+                        "clang",
+                        "clang-tools-extra",
+                        "libunwind",
+                        "libcxx",
+                        "libcxxabi",
+                        "lld",
                     ],
-                    checks_on_target = [
+                    # Suppress passing LLVM_ENABLE_RUNTIMES to CMake.
+                    # We get this list from the CMake cache file.
+                    enable_runtimes=None,
+                    cmake_definitions={
+                        "CMAKE_CXX_FLAGS"               : "-D__OPTIMIZE__",
+
+                        "LLVM_TARGETS_TO_BUILD"         : "AArch64",
+                        "TOOLCHAIN_TARGET_TRIPLE"       : "aarch64-unknown-linux-gnu",
+                        "DEFAULT_SYSROOT"               : "C:/buildbot/.aarch64-ubuntu",
+                        "ZLIB_ROOT"                     : "C:/buildbot/.zlib-win32",
+                        "LLVM_LIT_ARGS"                 : "-v -vv --threads=32",
+                        "REMOTE_TEST_HOST"              : util.Interpolate("%(prop:remote_test_host:-)s"),
+                        "REMOTE_TEST_USER"              : util.Interpolate("%(prop:remote_test_user:-)s"),
+                    },
+                    cmake_options=[
+                        "-C", util.Interpolate("%(prop:srcdir_relative)s/clang/cmake/caches/CrossWinToARMLinux.cmake")
+                    ],
+                    allow_cmake_defaults=True,
+                    checks=[
+                        "check-llvm",
+                        "check-clang",
+                        "check-lld",
+                        "check-compiler-rt-aarch64-unknown-linux-gnu"
+                    ],
+                    checks_on_target=[
                         ("libunwind",
                             ["python", "bin/llvm-lit.py",
                             "-v", "-vv", "--threads=32",
@@ -164,16 +224,15 @@ all = [
                             'runtimes/runtimes-aarch64-unknown-linux-gnu-bins/libcxx/test',
                             ])
                     ],
-                    extra_configure_args=[
-                        "-DLLVM_TARGETS_TO_BUILD=AArch64",
-                        "-DTOOLCHAIN_TARGET_TRIPLE=aarch64-unknown-linux-gnu",
-                        "-DDEFAULT_SYSROOT=C:/buildbot/.aarch64-ubuntu",
-                        "-DZLIB_ROOT=C:/buildbot/.zlib-win32",
-                        "-DLLVM_LIT_ARGS=-v -vv --threads=32",
-                        util.Interpolate("%(prop:remote_test_host:+-DREMOTE_TEST_HOST=)s%(prop:remote_test_host:-)s"),
-                        util.Interpolate("%(prop:remote_test_user:+-DREMOTE_TEST_USER=)s%(prop:remote_test_user:-)s"),
-                    ],
-                    cmake_cache="../llvm-project/clang/cmake/caches/CrossWinToARMLinux.cmake")},
+                    vs="autodetect",
+                    install_dir="install",
+                    clean=True,
+                    env = {
+                        # TMP/TEMP within the build dir (to utilize a ramdisk).
+                        'TMP'        : util.Interpolate("%(prop:builddir)s/%(prop:objdir)s"),
+                        'TEMP'       : util.Interpolate("%(prop:builddir)s/%(prop:objdir)s"),
+                    }
+                )},
 
 # LLD builders.
 
@@ -181,21 +240,25 @@ all = [
     'tags'  : ["lld"],
     'workernames' : ["as-worker-93"],
     'builddir': "lld-x86_64-win-rel",
-    'factory' : UnifiedTreeBuilder.getCmakeWithNinjaWithMSVCBuildFactory(
+    'factory' : UnifiedTreeBuilder.getCmakeExBuildFactory(
                     depends_on_projects=['llvm', 'lld'],
                     vs="autodetect",
-                    extra_configure_args = [
-                        '-DLLVM_ENABLE_WERROR=OFF'])},
+                    cmake_definitions={
+                        "LLVM_ENABLE_WERROR"            : "OFF",
+                    },
+                )},
 
     {'name' : "lld-x86_64-ubuntu-release",
     'tags'  : ["lld"],
     'workernames' : ["as-builder-4"],
     'builddir' : "lld-x86_64-ubuntu-rel",
-    'factory': UnifiedTreeBuilder.getCmakeWithNinjaBuildFactory(
-                            clean=True,
-                            extra_configure_args=[
-                                '-DLLVM_ENABLE_WERROR=OFF'],
-                            depends_on_projects=['llvm', 'lld'])},
+    'factory': UnifiedTreeBuilder.getCmakeExBuildFactory(
+                    depends_on_projects=['llvm', 'lld'],
+                    clean=True,
+                    cmake_definitions={
+                        "LLVM_ENABLE_WERROR"            : "OFF",
+                    },
+                )},
 
 # LTO and ThinLTO builders.
 
@@ -236,41 +299,45 @@ all = [
     'tags'  : ["llvm", "clang", "clang-tools-extra", "lld", "cross-project-tests"],
     'workernames' : ["sie-win-worker"],
     'builddir': "x86_64-sie-win-rel",
-    'factory' : UnifiedTreeBuilder.getCmakeWithNinjaWithMSVCBuildFactory(
+    'factory' : UnifiedTreeBuilder.getCmakeExBuildFactory(
                     vs="autodetect",
-                    target_arch='x64',
-                    depends_on_projects=['llvm','clang','clang-tools-extra','lld','cross-project-tests'],
+                    vs_arch='x64',
+                    depends_on_projects=['llvm', 'clang', 'clang-tools-extra', 'lld', 'cross-project-tests'],
                     clean=True,
-                    extra_configure_args=[
-                        "-DCMAKE_BUILD_TYPE=Release",
-                        "-DCLANG_ENABLE_ARCMT=OFF",
-                        "-DCLANG_ENABLE_CLANGD=OFF",
-                        "-DLLVM_CCACHE_BUILD=ON",
-                        "-DLLVM_DEFAULT_TARGET_TRIPLE=x86_64-sie-ps5",
-                        "-DLLVM_INCLUDE_EXAMPLES=OFF",
-                        "-DLLVM_TARGETS_TO_BUILD=X86",
-                        "-DLLVM_VERSION_SUFFIX=",
-                        "-DLLVM_BUILD_RUNTIME=OFF",
-                        "-DLLVM_ENABLE_ASSERTIONS=ON",
-                        "-DLLVM_LIT_ARGS=--verbose"])},
+                    cmake_definitions={
+                        "CMAKE_BUILD_TYPE"              : "Release",
+                        "CLANG_ENABLE_ARCMT"            : "OFF",
+                        "CLANG_ENABLE_CLANGD"           : "OFF",
+                        "LLVM_CCACHE_BUILD"             : "ON",
+                        "LLVM_DEFAULT_TARGET_TRIPLE"    : "x86_64-sie-ps5",
+                        "LLVM_INCLUDE_EXAMPLES"         : "OFF",
+                        "LLVM_TARGETS_TO_BUILD"         : "X86",
+                        "LLVM_VERSION_SUFFIX"           : "",       #TODO:VV: is this def should be empty?
+                        "LLVM_BUILD_RUNTIME"            : "OFF",
+                        "LLVM_ENABLE_ASSERTIONS"        : "ON",
+                        "LLVM_LIT_ARGS"                 : "--verbose",
+                    }
+                )},
 
     {'name': "llvm-clang-x86_64-gcc-ubuntu-release",
     'tags'  : ["llvm", "clang", "clang-tools-extra", "compiler-rt", "lld", "cross-project-tests"],
     'workernames': ["doug-worker-2a"],
     'builddir': "x86_64-gcc-rel",
-    'factory': UnifiedTreeBuilder.getCmakeWithNinjaBuildFactory(
-                    depends_on_projects=['llvm','clang','clang-tools-extra','compiler-rt','lld','cross-project-tests'],
-                    extra_configure_args=[
-                        "-DCMAKE_C_COMPILER=gcc",
-                        "-DCMAKE_CXX_COMPILER=g++",
-                        "-DCMAKE_BUILD_TYPE=Release",
-                        "-DCLANG_ENABLE_CLANGD=OFF",
-                        "-DLLVM_BUILD_RUNTIME=ON",
-                        "-DLLVM_BUILD_TESTS=ON",
-                        "-DLLVM_ENABLE_ASSERTIONS=ON",
-                        "-DLLVM_INCLUDE_EXAMPLES=OFF",
-                        "-DLLVM_LIT_ARGS=--verbose -j48",
-                        "-DLLVM_PARALLEL_LINK_JOBS=16",
-                        "-DLLVM_USE_LINKER=gold"])},
+    'factory': UnifiedTreeBuilder.getCmakeExBuildFactory(
+                    depends_on_projects=['llvm', 'clang', 'clang-tools-extra', 'compiler-rt', 'lld', 'cross-project-tests'],
+                    cmake_definitions={
+                        "CMAKE_C_COMPILER"              : "gcc",
+                        "CMAKE_CXX_COMPILER"            : "g++",
+                        "CMAKE_BUILD_TYPE"              : "Release",
+                        "CLANG_ENABLE_CLANGD"           : "OFF",
+                        "LLVM_BUILD_RUNTIME"            : "ON",
+                        "LLVM_BUILD_TESTS"              : "ON",
+                        "LLVM_ENABLE_ASSERTIONS"        : "ON",
+                        "LLVM_INCLUDE_EXAMPLES"         : "OFF",
+                        "LLVM_LIT_ARGS"                 : "--verbose -j48",
+                        "LLVM_PARALLEL_LINK_JOBS"       : "16",
+                        "LLVM_USE_LINKER"               : "gold",
+                    }
+                )},
 
 ]
